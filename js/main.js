4e46a7ec588eba6fd054f1fe28bf6d783deb33cf
Brayan Ruiz <brayanja00@hotmail.com>
2020-04-07 22:42:02 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/* Typed JS */
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="teclado">Creación</i>',
+		'<i class="teclado">Diseño</i>',
+		'<i class="teclado">Proyectos</i>'
+	],
+
+	typeSpeed: 40, // Velocidad de escritura
+	startDelay: 500, // Tiempo para empezar a escribir cuando se recarga la pagina
+	backSpeed: 45, // Velocidad de borrado
+	shuffle: false, // Alterar el orden en que se escriben las palabras
+	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra
+	loop: true, // Repetir el array de strings
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|'
+});
+
+/* Slider */
+$('.galeria').bxSlider({
+	auto: true,
+	mode: 'fade',
+	captions: false,
+	slideWidth: 500,
+	responsive: true,
+	pager: false,
+	controls: false,
+	speed: 1000,
+});
+
+//Codigo javascript para agregar la funcion del boton claro/oscuro
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active-button');
+
+	// Guardamos el modo en localStorage
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	}else{
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active-button');
+}else{
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active-button');
+}
+
+// Menú desplegable
+$(document).ready(() => {
+
+	$('.scroll-top').click((e) => {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+
+		return false;
+	});
+	
+});
---
+++ js/portafolio.js
@@ -0,0 +1,91 @@
+const grid = new Muuri('.grid',{
+	layout: {
+    rounding: false
+  }
+});
+
+window.addEventListener('load', () => {
+	grid.refreshItems().layout();
+	document.getElementById('grid').classList.add('imagenes-cargadas');
+
+	// Agregamos los listener de los enlaces para filtrar por categoria.
+	const enlaces = document.querySelectorAll('#categorias a');
+	enlaces.forEach((elemento) => {
+		elemento.addEventListener('click', (evento) => {
+			evento.preventDefault();
+			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+			evento.target.classList.add('activo');
+
+			const categoria = evento.target.innerHTML.toLowerCase();
+			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+		});
+	});
+
+	// Agregamos el listener para la barra de busqueda
+	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+		const busqueda = evento.target.value;
+		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+	});
+
+	// Agregamos el listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
+
+	// EventListener del boton de cerrar
+	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+		overlay.classList.remove('activo');
+	});
+
+	// EventListener del overlay
+	overlay.addEventListener('click', (evento) => {
+		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+	});
+});
+
+//Codigo javascript para agregar la funcion del boton claro/oscuro
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active-button');
+
+	// Guardamos el modo en localStorage
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	}else{
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active-button');
+}else{
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active-button');
+}
+
+// Menú desplegable
+$(document).ready(() => {
+
+	$('.scroll-top').click((e) => {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+
+		return false;
+	});
+
+});
